106134f3ae713c2adec6a9a1659ed4a961ed2660
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2019-10-09 02:20:18 +0500

    worked on signup controlled components

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 
 const Signup = () => {
 
+    let [firstName, setFirstName] = useState("");
+    let [lastName, setLastName] = useState("");
+    let [emailAddress, setEmailAddress] = useState("");
+    let [password, setPassword] = useState("");
+
 
     return (
         <React.Fragment>
-            
+
             <form>
                 <div className='container'>
                     <h1>Sign Up</h1>
@@ -20,7 +25,9 @@ const Signup = () => {
                             <label>First Name</label>
                         </div>
                         <div className='col-3'>
-                            <input type='text' className='form-control'></input>
+                            <input type='text' value={firstName} onChange={(e) => {
+                                setFirstName(e.target.value)
+                            }} className='form-control'></input>
                         </div>
                     </div>
                     <div className='row form-group'>
@@ -28,7 +35,9 @@ const Signup = () => {
                             <label>Last Name</label>
                         </div>
                         <div className='col-3'>
-                            <input type='text' className='form-control'></input>
+                            <input type='text' value={lastName} onChange={(e) => {
+                                setLastName(e.target.value)
+                            }} className='form-control'></input>
                         </div>
                     </div>
                     <div className='row form-group'>
@@ -36,7 +45,9 @@ const Signup = () => {
                             <label>Email Address</label>
                         </div>
                         <div className='col-3'>
-                            <input type='text' className='form-control'></input>
+                            <input type='text' value={emailAddress} onChange={(e) => {
+                                setEmailAddress(e.target.value)
+                            }}  className='form-control'></input>
                         </div>
                     </div>
                     <div className='row form-group'>
@@ -44,9 +55,11 @@ const Signup = () => {
                             <label>Password</label>
                         </div>
                         <div className='col-3'>
-                            <input type='password' className='form-control'></input>
+                            <input type='password' value={password} onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}  className='form-control'></input>
                         </div>
-                    </div>     
+                    </div>
                     <div className='row form-group'>
                         <div className='col-3'>
                             <label>Gender</label>
@@ -68,17 +81,17 @@ const Signup = () => {
                                 <option>B</option>
                                 <option>AB</option>
                                 <option>O</option>
-                                
+
                             </select>
                         </div>
                     </div>
-                    <div className='row form-group'>      
+                    <div className='row form-group'>
                         <div className='col-3'>
                             <Link to='/login'>
-                            <button className="btn btn-primary" >Sign up</button>
+                                <button className="btn btn-primary" >Sign up</button>
                             </Link>
                             <small className="form-text text-muted">Already have a account? <Link to='/login'>Login </Link> now</small>
-  
+
                         </div>
                     </div>
                 </div>
